69271a46df95a3024bb769f6114ce9436b84a740
Egor Ezhkov <egorkashpak21@yandex.ru>
2025-06-28 17:07:41 +0300

    start a make welcomeSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
-import Header from './components/Header/Header'
+import "./App.css";
+import Header from "./components/Header/Header";
+import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
 function App() {
   return (
     <>
       <Header></Header>
+      <WelcomeSection></WelcomeSection>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import styles from "./WelcomeSection.module.css";
+
+const WelcomeSection: FC = () => {
+  return (
+    <>
+      <div className={styles.divContainer}></div>
+    </>
+  );
+};
+
+export default WelcomeSection;
