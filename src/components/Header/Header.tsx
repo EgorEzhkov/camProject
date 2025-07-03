fb69d5263f8391790dda1cf5b830e979181f153f
Egor Ezhkov <egorkashpak21@yandex.ru>
2025-07-03 18:40:02 +0300

    in progress

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import Button from "../../ui/Button/Button";
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.png";
+import joinIcon from "../../assets/images/joinIcon.svg";
+import { useState } from "react";
 
 const Header = () => {
   const buttonProps = {
@@ -11,14 +13,25 @@ const Header = () => {
     padding: "7px 20px",
   } as const;
 
+  const [onClickBurger, setOnClickBurger] = useState(false);
+  console.log(onClickBurger);
   return (
     <>
       <header className={styles.header}>
+        <div
+          className={`${styles.navIcon} ${onClickBurger ? styles.active : ""}`}
+          onClick={() => setOnClickBurger(!onClickBurger)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
         <div className={styles.imgContainer}>
           <img src={logo} alt="" />
         </div>
         <div className={styles.buttonsContainer}>
-          <ul className={styles.ul}>
+          <ul className={`${styles.ul} ${onClickBurger ? styles.active : ""}`}>
             <li className={styles.li}>
               <Button {...buttonProps}>Готовые решения</Button>
             </li>
@@ -41,6 +54,9 @@ const Header = () => {
             >
               Войти
             </Button>
+          </div>
+          <div className={styles.joinIconContainer}>
+            <img src={joinIcon} alt="" />
           </div>
         </div>
       </header>
