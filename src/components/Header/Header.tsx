cba3e393e6418e2c6a605633b6d8ee89b978f94e
Egor Ezhkov <egorkashpak21@yandex.ru>
2025-07-04 16:15:46 +0300

    start adaptive for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,52 +2,120 @@ import Button from "../../ui/Button/Button";
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.png";
 import joinIcon from "../../assets/images/joinIcon.svg";
-import { useState } from "react";
+import phoneIcon from "../../assets/images/phoneIcon.svg";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { RemoveScrollBar } from "react-remove-scroll-bar";
+
+const buttonPropsDesctop = {
+  fontFamily: "Montserrat",
+  fontWeight: "Regular",
+  fontSize: 16,
+  backgroundColor: false,
+  padding: "7px 20px",
+} as const;
+
+const buttonPropsMobile = {
+  fontFamily: "Montserrat",
+  fontWeight: "ExtraBold",
+  fontSize: 20,
+  backgroundColor: false,
+  padding: "0",
+  color: "black",
+} as const;
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const Header = () => {
-  const buttonProps = {
-    fontFamily: "Montserrat",
-    fontWeight: "Regular",
-    fontSize: 16,
-    backgroundColor: false,
-    padding: "7px 20px",
-  } as const;
+  const [menuState, setMenuState] = useState<"closed" | "opening" | "closing">(
+    "closed"
+  );
+  const ulRef = useRef<HTMLUListElement>(null);
 
-  const [onClickBurger, setOnClickBurger] = useState(false);
-  console.log(onClickBurger);
+  const handleMenu = useCallback(async () => {
+    if (menuState === "closed") {
+      setMenuState("opening");
+    } else if (menuState === "opening") {
+      setMenuState("closing");
+      await delay(300); // ровно столько нужно для анимации
+      setMenuState("closed");
+    }
+  }, [menuState]);
+
+  useEffect(() => {
+    if (menuState === "closed") return;
+
+    const handleClickOutSide = (event: MouseEvent) => {
+      if (ulRef.current && !ulRef.current.contains(event.target as Node)) {
+        handleMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutSide);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutSide);
+    };
+  }, [menuState, handleMenu]);
+
+  const propsForButton = {
+    ...(menuState === "opening"
+      ? { ...buttonPropsMobile }
+      : menuState === "closed"
+      ? { ...buttonPropsDesctop }
+      : { ...buttonPropsMobile }),
+  };
+
   return (
     <>
       <header className={styles.header}>
         <div
-          className={`${styles.navIcon} ${onClickBurger ? styles.active : ""}`}
-          onClick={() => setOnClickBurger(!onClickBurger)}
+          className={`${styles.navIcon} ${
+            menuState === "opening" ? styles.active : ""
+          }`}
+          onClick={handleMenu}
         >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span className={menuState === "opening" ? styles.active : ""}></span>
+          <span className={menuState === "opening" ? styles.active : ""}></span>
+          <span className={menuState === "opening" ? styles.active : ""}></span>
+          <span className={menuState === "opening" ? styles.active : ""}></span>
         </div>
         <div className={styles.imgContainer}>
           <img src={logo} alt="" />
         </div>
         <div className={styles.buttonsContainer}>
-          <ul className={`${styles.ul} ${onClickBurger ? styles.active : ""}`}>
-            <li className={styles.li}>
-              <Button {...buttonProps}>Готовые решения</Button>
-            </li>
-            <li className={styles.li}>
-              <Button {...buttonProps}>Тарифы</Button>
-            </li>
-            <li className={styles.li}>
-              <Button {...buttonProps}>Новости</Button>
-            </li>
-            <li className={styles.li}>
-              <Button {...buttonProps}>Контакты</Button>
-            </li>
+          {menuState === "opening" && <RemoveScrollBar></RemoveScrollBar>}
+          <ul
+            className={`${styles.ul} ${
+              menuState === "opening"
+                ? styles.active
+                : menuState === "closing"
+                ? styles.inactive
+                : ""
+            }`}
+            ref={ulRef}
+          >
+            {["Готовые решения", "Тарифы", "Новости", "Контакты"].map(
+              (el, index) => {
+                return (
+                  <li className={styles.li} key={index}>
+                    <Button {...propsForButton}>{el}</Button>
+                  </li>
+                );
+              }
+            )}
+
+            {menuState === "opening" && (
+              <div className={styles.phoneNumberAndIconContainer}>
+                <a href="tel:+74732573191" className={styles.phoneNumber}>
+                  +7 (473) 257-31-91
+                </a>
+                <img src={phoneIcon} alt="phoneicon" />
+              </div>
+            )}
           </ul>
           <div className={styles.buttonJoinContainer}>
             <Button
-              {...buttonProps}
+              {...buttonPropsDesctop}
               border={true}
               borderColor="purple"
               padding="7px 30px"
@@ -56,7 +124,7 @@ const Header = () => {
             </Button>
           </div>
           <div className={styles.joinIconContainer}>
-            <img src={joinIcon} alt="" />
+            <img src={joinIcon} alt="joinIcon" />
           </div>
         </div>
       </header>
