8cc8f3bdc7dbe017e90af57a3b24b7cc2885455a
Egor Ezhkov <egorkashpak21@yandex.ru>
2025-07-04 21:13:53 +0300

    header adaptive is ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,42 +1,36 @@
-import Button from "../../ui/Button/Button";
 import styles from "./Header.module.css";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+
+import { RemoveScrollBar } from "react-remove-scroll-bar";
+
+import Button from "../../ui/Button/Button";
 import logo from "../../assets/images/logo.png";
 import joinIcon from "../../assets/images/joinIcon.svg";
 import phoneIcon from "../../assets/images/phoneIcon.svg";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { RemoveScrollBar } from "react-remove-scroll-bar";
 
-const buttonPropsDesctop = {
-  fontFamily: "Montserrat",
-  fontWeight: "Regular",
-  fontSize: 16,
-  backgroundColor: false,
-  padding: "7px 20px",
-} as const;
-
-const buttonPropsMobile = {
-  fontFamily: "Montserrat",
-  fontWeight: "ExtraBold",
-  fontSize: 20,
-  backgroundColor: false,
-  padding: "0",
-  color: "black",
-} as const;
-
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+import { delay } from "../../utils/utils";
+import { buttonPropsDesctop, buttonPropsMobile } from "../../utils/constants";
 
 const Header = () => {
   const [menuState, setMenuState] = useState<"closed" | "opening" | "closing">(
     "closed"
   );
-  const ulRef = useRef<HTMLUListElement>(null);
+  console.log(menuState);
 
-  const handleMenu = useCallback(async () => {
+  const propsForButton =
+    menuState !== "closed" ? buttonPropsMobile : buttonPropsDesctop;
+
+  const ulRef = useRef<HTMLUListElement | null>(null);
+
+  const handleHeaderMenu = useCallback(async () => {
     if (menuState === "closed") {
       setMenuState("opening");
     } else if (menuState === "opening") {
       setMenuState("closing");
-      await delay(300); // ровно столько нужно для анимации
+      await delay(280); // на анимацию тратится 300мс, поставил меньше,
+      // потому что так меньше лагов при анимации. Надо бы вообще это всё по-другому сделать,
+      // но пока так
       setMenuState("closed");
     }
   }, [menuState]);
@@ -46,7 +40,7 @@ const Header = () => {
 
     const handleClickOutSide = (event: MouseEvent) => {
       if (ulRef.current && !ulRef.current.contains(event.target as Node)) {
-        handleMenu();
+        handleHeaderMenu();
       }
     };
 
@@ -55,35 +49,39 @@ const Header = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutSide);
     };
-  }, [menuState, handleMenu]);
-
-  const propsForButton = {
-    ...(menuState === "opening"
-      ? { ...buttonPropsMobile }
-      : menuState === "closed"
-      ? { ...buttonPropsDesctop }
-      : { ...buttonPropsMobile }),
-  };
+  }, [menuState, handleHeaderMenu]);
 
   return (
     <>
       <header className={styles.header}>
+        {menuState === "closing" || menuState === "opening" ? (
+          <div
+            className={`${styles.overlay} ${
+              menuState === "opening"
+                ? styles.active
+                : menuState === "closing"
+                ? styles.inactive
+                : ""
+            }`}
+          ></div>
+        ) : null}
         <div
           className={`${styles.navIcon} ${
             menuState === "opening" ? styles.active : ""
           }`}
-          onClick={handleMenu}
+          onClick={handleHeaderMenu}
         >
           <span className={menuState === "opening" ? styles.active : ""}></span>
           <span className={menuState === "opening" ? styles.active : ""}></span>
           <span className={menuState === "opening" ? styles.active : ""}></span>
           <span className={menuState === "opening" ? styles.active : ""}></span>
         </div>
+
         <div className={styles.imgContainer}>
           <img src={logo} alt="" />
         </div>
         <div className={styles.buttonsContainer}>
-          {menuState === "opening" && <RemoveScrollBar></RemoveScrollBar>}
+          {menuState !== "closed" && <RemoveScrollBar />}
           <ul
             className={`${styles.ul} ${
               menuState === "opening"
@@ -94,16 +92,17 @@ const Header = () => {
             }`}
             ref={ulRef}
           >
-            {["Готовые решения", "Тарифы", "Новости", "Контакты"].map(
-              (el, index) => {
-                return (
-                  <li className={styles.li} key={index}>
-                    <Button {...propsForButton}>{el}</Button>
-                  </li>
-                );
-              }
-            )}
-
+            <div className={styles.list}>
+              {["Готовые решения", "Тарифы", "Новости", "Контакты"].map(
+                (el) => {
+                  return (
+                    <li className={styles.li} key={el}>
+                      <Button {...propsForButton}>{el}</Button>
+                    </li>
+                  );
+                }
+              )}
+            </div>
             {menuState === "opening" && (
               <div className={styles.phoneNumberAndIconContainer}>
                 <a href="tel:+74732573191" className={styles.phoneNumber}>
@@ -123,9 +122,9 @@ const Header = () => {
               Войти
             </Button>
           </div>
-          <div className={styles.joinIconContainer}>
-            <img src={joinIcon} alt="joinIcon" />
-          </div>
+        </div>
+        <div className={styles.joinIconContainer}>
+          <img src={joinIcon} alt="joinIcon" />
         </div>
       </header>
     </>
