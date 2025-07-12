4fdb60e598eeaf7ac4446e4f62b827c2784e4a37
Egor Ezhkov <egorkashpak21@yandex.ru>
2025-07-12 12:05:51 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import NoPage from "./pages/NoPage/NoPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/camProject" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
 
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
